a7df1bee7d887ff3727905c99216540ec4cd210b
daveprowse <65040686+daveprowse@users.noreply.github.com>
2020-12-16 01:11:03 -0500

    Update kea-install-script.sh

--- kea-install-script.sh
+++ kea-install-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this script is designed to install the ISC Kea DHCP4 server to a Debian Linux system.
 # run this as root (for testing purposes). 
-Otherwise, your account will need to be a sudoer and you will need to add `sudo` before `bash` on line 12, and at the beggining of lines 10, 11, and 13. 
+# Otherwise, your account will need to be a sudoer and you will need to add `sudo` before `bash` on line 12, and at the beggining of lines 10, 11, and 13. 
 # make sure you chmod +x this script before running.
 # this script installs kea-1-8, for newer versions, modify line 12
  
